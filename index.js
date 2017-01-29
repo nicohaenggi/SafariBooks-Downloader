00140b5010383e3af681b8382b5abbacb1a2138c
Nico Haenggi <nico@haenggi.ch>
2017-01-29 18:42:36 +0100

    added shell suport

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// # Application Bootup
+// boots up the application
+
+// defines that javascript should be executed in 'strict mode' (ECMAScript 5)
+'use strict';
+
+// ## Initialise CLI
+// imports the Command Line Interface (CLI) in order to make the application work
+module.exports = require('./lib/cli/index.js');
