90922c23f09dd1827483fca7f6e15ab6aa823591
Nico Haenggi <nico@haenggi.ch>
2017-01-30 14:23:39 +0100

    added safari client and authorization request

--- index.js
+++ index.js
@@ -6,6 +6,12 @@
 // defines that javascript should be executed in 'strict mode' (ECMAScript 5)
 'use strict';
 
-// ## Initialise CLI
+// # Initialise CLI
 // imports the Command Line Interface (CLI) in order to make the application work
 module.exports = require('./lib/cli/index.js');
+
+// # Closing Application
+// make sure the manager gets stopped
+process.on( 'SIGINT', function() {
+  process.exit( );
+});
